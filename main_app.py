71632d0bab5ac9ad2cec01018c42fc8272f142ad
nos <levin024@gmail.com>
2026-07-01 16:33:27 +0400

    fix for hanging downloads threads

--- main_app.py
+++ main_app.py
@@ -1,6 +1,16 @@
 from gui import DownloaderApp
+import sys
+import io
+import multiprocessing
+
+# Fix the --noconsole stream crashes
+if sys.stdout is None or isinstance(sys.stdout, io.StringIO) is False:
+    sys.stdout = io.StringIO()
+if sys.stderr is None or isinstance(sys.stderr, io.StringIO) is False:
+    sys.stderr = io.StringIO()
 
 def main():
+    multiprocessing.freeze_support()
     app = DownloaderApp()
     app.mainloop()
 
